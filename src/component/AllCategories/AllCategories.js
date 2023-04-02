5926357886f11b64d3d3cabd7ef1d4913d559b65
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-03 03:08:04 +0600

    make layout responsive , load data and show them UI

--- src/component/AllCategories/AllCategories.js
+++ src/component/AllCategories/AllCategories.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Common/Header';
 import { Container, Col, Row } from 'react-bootstrap';
+import SingleCategories from '../SingleCategories/SingleCategories';
 
 const AllCategories = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch("fakeDb.json")
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
         <div>
             <Container fluid>
                 <Row>
                     <Col><Header /></Col>
+                </Row>
+
+                <Row>
+                    <Col lg={8} sm={6} xl={9}>
+                        <Container fluid className='my-5'>
+                            <Row>
+
+                                {
+                                    categories.map(data => <Col xl={4} className='gy-3'><SingleCategories key={data._id} data={data} /></Col>)
+                                }
+
+                            </Row>
+                        </Container>
+                    </Col>
+                    <Col lg={4} sm={6} xl={3} className='bg-secondary'>
+                        <h1>another colum for details</h1>
+                    </Col>
                 </Row>
             </Container>
         </div>
--- src/component/SingleCategories/SingleCategories.js
+++ src/component/SingleCategories/SingleCategories.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Button, Card, ListGroup } from 'react-bootstrap';
 
 
-const SingleCategories = () => {
+const SingleCategories = (props) => {
+    const { name, age, description, picture, time } = props.data;
     return (
-        <div>
-            <h1>single item</h1>
-        
+        <div >
+            <Card className='text-start p-2 ' >
+                <Card.Img className='img-custom-size' style={{ height: "100px" }} variant="top" src={picture} />
+                <Card.Body>
+                    <Card.Title> <span className="badge bg-danger">{name} </span></Card.Title>
+                    <Card.Text >{description.slice(0, 60)}</Card.Text>
+                    <ListGroup.Item className='fw-semibold' >For Age : <span className="badge bg-secondary  rounded-pill">{age} </span> years </ListGroup.Item>
+                    <ListGroup.Item className='fw-semibold' >Time require : <span className="badge  rounded-pill bg-secondary"> {time} </span> seconds </ListGroup.Item>
+                    <Button className='mt-2' variant="outline-secondary">Add to list</Button>
+                </Card.Body>
+            </Card>
+
         </div>
     );
 };
