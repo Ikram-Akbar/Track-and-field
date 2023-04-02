81d7970e980c1f34b2a55d296df74b924b22f70f
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-03 00:12:50 +0600

    component create - Categories

---
+++ src/component/AllCategories/AllCategories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AllCategories = () => {
+    return (
+        <div>
+            <h1>All Categories </h1>
+            
+        </div>
+    );
+};
+
+export default AllCategories;
